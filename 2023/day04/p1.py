5a5af7246064ee65cd000c9853c1403ed0b1f90a
Jason Price <jprice@squareup.com>
2023-12-04 09:52:09 -0500

    cleanup code and comment out debug helpers

--- 2023/day04/p1.py
+++ 2023/day04/p1.py
@@ -5,7 +5,7 @@ import collections
 import typing
 
 
-def parseargs() -> argparse.Namespace:
+def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="input file")
     return parser.parse_args()
@@ -13,7 +13,7 @@ def parseargs() -> argparse.Namespace:
 
 def get_input(in_file: str) -> typing.List[str]:
     with open(in_file) as f:
-        return [line.strip() for line in f.readlines()]
+        return f.read().splitlines()
 
 
 def parse_row(row: str) -> typing.Tuple[int, int, int]:
@@ -22,37 +22,26 @@ def parse_row(row: str) -> typing.Tuple[int, int, int]:
     split2 = split1[1].split(" | ")
     winners = split2[0].split()
     attempts = split2[1].split()
-    tally = sum([1 for attempt in attempts if attempt in winners])
-    my_winners = [attempt for attempt in attempts if attempt in winners]
-    my_winners.sort()
-    if tally == 0:
-        result = 0
+    num_winners = sum([1 for attempt in attempts if attempt in winners])
+    # my_winners = [attempt for attempt in attempts if attempt in winners]
+    # my_winners.sort()
+    if num_winners == 0:
+        winner_value = 0
     else:
-        result = 2 ** (tally - 1)
+        winner_value = 2 ** (num_winners - 1)
     # print(f"{card}: {winners} {attempts} {my_winners} {tally} {result}")
-    return int(card), tally, result
+    return int(card), num_winners, winner_value
 
-
-# 1
-# 2 1c
-# 3 1c 2c
-# 4 1c 2c 4c
-# 5 1c    4c 8c
-# 6
 
 def main() -> None:
-    args = parseargs()
+    args = parse_args()
     data = get_input(args.input)
     p1_tally = 0
     p2_tally = 0
     copies: typing.Dict[int, int] = collections.defaultdict(lambda: 1)
     copies[1] = 1
     for row in data:
-        try:
-            card, num_winners, winner_value = parse_row(row)
-        except ValueError:
-            print(f"failed to parse row {row}")
-            raise
+        card, num_winners, winner_value = parse_row(row)
         p1_tally += winner_value
 
         card_count = copies[card]
@@ -62,7 +51,6 @@ def main() -> None:
             # nctc = copies[t_card]
             # print(f"due to card {card} added {card_count} to card {t_card} - old value {ctc} new value {nctc}")
     for k, v in copies.items():
-        # print(f"card {k}: {v}")
         p2_tally += v
 
     print(f"Solution: Part 1: {p1_tally} Part 2: {p2_tally}")
