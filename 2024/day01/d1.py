99c89ecc84d7ddd9c4a135bfe9757e94bf2c9042
Jason Price <jprice@squareup.com>
2024-12-01 09:00:06 -0500

    2024 d1

---
+++ 2024/day01/d1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import collections
+import fileinput
+
+def main() -> None:
+    # parse input
+    col1, col2 = [], []
+    count_col2 = collections.defaultdict(int)
+    for line in fileinput.input():
+        n1, n2 = map(int, line.split())
+        col1.append(n1)
+        col2.append(n2)
+        count_col2[n2] += 1
+    col1.sort()
+    col2.sort()
+
+    # part 1
+    tally = 0
+    for n1, n2 in zip(col1, col2):
+        tally += abs(n1 - n2)
+    print("part1: ", tally)
+
+    # part 2
+    tally = 0
+    for n1 in col1:
+        c = count_col2[n1]
+        tally += (n1 * c)
+    print("part2: ", tally)
+
+
+if __name__ == '__main__':
+    main()
