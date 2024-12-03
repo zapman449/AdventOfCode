aa72aa24420c9a35e09639a0ead463fc86116d78
Jason Price <jprice@squareup.com>
2024-12-03 10:06:46 -0500

    abstract out string lengths and reformat

--- 2024/day03/d3.py
+++ 2024/day03/d3.py
@@ -7,13 +7,13 @@ import re
 # 161 (2*4 + 5*5 + 11*8 + 8*5)
 # 48 (2*4 + 8*5)
 
+
 def calc_subline(subline: str) -> int:
     tally: int = 0
-    mul = re.compile(r'mul\((\d+),(\d+)\)')
+    mul = re.compile(r"mul\((\d+),(\d+)\)")
     matches = mul.findall(subline)
     for match in matches:
-        # print(repr(match))
-        x , y = map(int, match)
+        x, y = map(int, match)
         tally += x * y
     return tally
 
@@ -22,31 +22,32 @@ def main() -> None:
     p1_tally: int = 0
     p2_tally: int = 0
     do: bool = True
+    dostr = "do()"
+    dontstr = "don't()"
     for line in fileinput.input():
         p1_tally += calc_subline(line)
 
         while True:
             if do:
-                off_idx = line.find("don't()")
+                off_idx = line.find(dontstr)
                 if off_idx == -1:
                     p2_tally += calc_subline(line)
                     break
                 else:
                     p2_tally += calc_subline(line[:off_idx])
-                    line = line[off_idx + 7:]
+                    line = line[off_idx + len(dontstr) :]
                     do = False
             else:
-                on_idx = line.find("do()")
+                on_idx = line.find(dostr)
                 if on_idx == -1:
                     break
                 else:
-                    line = line[on_idx + 4:]
+                    line = line[on_idx + len(dostr) :]
                     do = True
 
     print("p1: ", p1_tally)
     print("p2: ", p2_tally)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
